2447f56dfba77a32c5dec51fea1f3f50f6e7cd95
xTerradon <l-b-n.02@gmx.de>
2022-10-19 13:54:36 +0200

    new entries

---
+++ collector/sqlite_execution.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+
+def add_new(name):
+    if name == "captchas":
+        return
+
+    table_name = name.replace(" ","_")
+
+    con = sqlite3.connect(table_name+".db")
+    cur = con.cursor()
+
+    cur.execute("""CREATE TABLE IF NOT EXISTS """+table_name+"""(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            captcha_string TEXT NOT NULL,
+            captcha_type TEXT NOT NULL,
+            image BLOB NOT NULL)""")
+    #cur.execute("DROP TABLE duck")
+    #cur.execute("DELETE FROM captchas WHERE captcha_string = \'duck\'")
+    cur.execute("INSERT INTO "+table_name+" SELECT * FROM captchas WHERE captcha_string = \'"+name+"\'")
+    cur.execute("DROP TABLE captchas")
+    con.commit()
+
+    con2 = sqlite3.connect("captchas.db")
+    cur2 = con2.cursor()
+
+    cur2.execute("DELETE FROM captchas WHERE captcha_string = \'"+name+"\'")
+    con2.commit()
+
+def vacuum(name):
+    con = sqlite3.connect(name+".db")
+    con.execute("VACUUM")
+    con.commit()
+
+#add_new("cactus in a pot")
