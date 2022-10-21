47e4e57366431bbff5d4e3b143f281fa33ac487d
xTerradon <l-b-n.02@gmx.de>
2022-10-21 12:44:55 +0200

    specific captcha string selection

--- manual_classifier/manual_classifier.py
+++ manual_classifier/manual_classifier.py
@@ -1,7 +1,9 @@
 import sqlite3
 import tkinter as tk
 from io import BytesIO
+from tkinter import ttk
 
+import numpy as np
 from PIL import Image, ImageTk
 
 
@@ -11,19 +13,28 @@ class Manual_Classifier:
         self.con = sqlite3.connect(self.name+".db")
         self.id_history = []
         print("Opened Table",self.name)
-        self.all_data = False
+    
+    def get_unique_captcha_strings(self):
+        return list(np.array(self.con.execute("SELECT DISTINCT captcha_string FROM "+self.name).fetchall())[:,0])
     
     def get_data(self):
-        self.number_total = self.con.execute("SELECT COUNT(rowid) FROM "+self.name).fetchone()[0]
-        self.number_unclassified = self.con.execute("SELECT COUNT(rowid) FROM "+self.name+" WHERE correct = 0").fetchone()[0]
-        if self.all_data:
+        
+        if self.captcha_selection.get() == "ALL":
             if len(self.id_history) == 0:
                 max_id = 0
             else:
                 max_id = max(self.id_history)
+            self.number_total = self.con.execute("SELECT COUNT(rowid) FROM "+self.name).fetchone()[0]
+            self.number_unclassified = self.con.execute("SELECT COUNT(rowid) FROM "+self.name+" WHERE correct = 0").fetchone()[0]
             element = self.con.execute("SELECT id, captcha_string, image, correct FROM "+self.name+" WHERE id > "+str(max_id)+" ORDER BY captcha_string LIMIT 1").fetchone()
         else:
-            element = self.con.execute("SELECT id, captcha_string, image, correct FROM "+self.name+" WHERE captcha_string = \'penguin on ice\' AND correct = 0 ORDER BY captcha_string LIMIT 1").fetchone()
+            cs = self.captcha_selection.get()
+            self.number_total = self.con.execute("SELECT COUNT(rowid) FROM "+self.name+" WHERE captcha_string = \'"+cs+"\'").fetchone()[0]
+            self.number_unclassified = self.con.execute("SELECT COUNT(rowid) FROM "+self.name+" WHERE captcha_string = \'"+cs+"\' AND correct = 0").fetchone()[0]
+            element = self.con.execute("SELECT id, captcha_string, image, correct FROM "+self.name+" WHERE captcha_string = \'"+cs+"\' AND correct = 0 ORDER BY captcha_string LIMIT 1").fetchone()
+        if len(element) == 0:
+            print("Could not resolve",self.captcha_selection.get())
+            return
         self.img_id = element[0]
         self.id_history.append(self.img_id)
         self.captcha_string = element[1]
@@ -35,7 +46,15 @@ class Manual_Classifier:
 
         self.window = tk.Tk()
         self.window.bind("<Key>",self.key_input)
+
         self.info_label = tk.Label(self.window, text="Loading...")
+        self.captcha_selection = tk.StringVar()
+        self.captcha_selector = ttk.Combobox(self.window, textvariable=self.captcha_selection)
+        print(self.get_unique_captcha_strings())
+        self.captcha_selector['values'] = ["ALL"] + self.get_unique_captcha_strings()
+        self.captcha_selector.config(state="readonly")
+        self.captcha_selector.bind('<<ComboboxSelected>>', self.refresh_data)
+        self.captcha_selection.set("ALL")
         self.captcha_label = tk.Label(self.window, text="Loading...")
 
         self.img_label = tk.Label()
@@ -48,6 +67,7 @@ class Manual_Classifier:
 
         revise_button = tk.Button(self.window, text="GO BACK", command=lambda : self.load_last(), width=15, height=2)
 
+        self.captcha_selector.pack(padx=5, pady=5)
         self.info_label.pack(padx=50)
         self.captcha_label.pack(padx=20, pady=5)
         self.img_label.pack(padx=20, pady=5)
@@ -74,13 +94,17 @@ class Manual_Classifier:
         else:
             print("KEYSTROKE NOT RECOGNIZED")
     
+    def refresh_data(self, _):
+        self.get_data()
+        self.load_data_to_ui()
+
     def update_correct_value(self, value):
         self.set_correct_value(value)
         self.get_data()
         self.load_data_to_ui()
     
     def load_data_to_ui(self):
-        self.info_label.config(text = "ID: "+str(self.img_id)+" "+str(self.current_correct)+" | Total: "+str(self.number_total)+" | Labeled: "+str(round(100*(self.number_total-self.number_unclassified)/self.number_total,2))+"%")
+        self.info_label.config(text = "ID: "+str(self.img_id)+" "+str(self.current_correct)+" | Total: "+str(self.number_total)+" | Labeled: "+str(self.number_total-self.number_unclassified)+" "+str(round(100*(self.number_total-self.number_unclassified)/self.number_total))+"%")
         self.captcha_label.config(text=self.captcha_string)
         self.img_label.config(image = self.captcha_image)
         self.img_label.image = self.captcha_image
