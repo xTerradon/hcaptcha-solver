02f8feecc95d4f8601a7ed8b67ca88c7abbd0e55
xTerradon <l-b-n.02@gmx.de>
2022-10-22 22:28:37 +0200

    refactoring + improvements

--- automatic_classifier/automatic_classifier.py
+++ automatic_classifier/automatic_classifier.py
@@ -3,40 +3,31 @@ import wd_handler
 
 import numpy as np
 
+class Captcha_Solver:
+    def __init__(self, website_url):
+        self.nh = nn_handler.NN_Handler()
+        available_models = self.nh.get_all_models()
+        self.wh = wd_handler.Webdriver_Handler(website_url,available_models)
+    
+    def solve_captcha(self):
+        self.wh.load_captcha()
+
+        while self.wh.get_verification_status() == False:
+            captcha_string = self.wh.get_string()
+            while self.nh.has_model(captcha_string) == False:
+                self.wh.click_refresh()
+                captcha_string= self.wh.get_string()
+            
+            images = self.wh.get_images()
+            predictions = self.nh.predict_images(captcha_string, images)
+            print(np.round(predictions.reshape((3,3)),2))
+
+            self.wh.click_correct_images(predictions)
+        print("Succesfully solved captcha!")
 
 if __name__ == "__main__":
-    nnh = nn_handler.NN_Handler()
-    available_models = nnh.get_all_models()
+    cs = Captcha_Solver("https://accounts.hcaptcha.com/demo")
+    cs.solve_captcha()
     #wdh = wd_handler.Webdriver_Handler("https://accounts.hcaptcha.com/demo")
-    wdh = wd_handler.Webdriver_Handler("https://freebitco.in/login",available_models)
-
-    wdh.load_captcha()
-
-    wdh.focus_on_captcha_frame()
-    captcha_string = wdh.get_string()
-    while nnh.has_model(captcha_string) == False:
-        wdh.click_refresh()
-        captcha_string= wdh.get_string()
-    
-    images = wdh.get_images()
-    predictions = nnh.predict_images(captcha_string, images)
-    print(np.round(predictions.reshape((3,3)),2))
-
-    wdh.click_correct_images(predictions)
-
-    wdh.focus_on_captcha_frame()
-    captcha_string = wdh.get_string()
-    while nnh.has_model(captcha_string) == False:
-        wdh.click_refresh()
-        captcha_string= wdh.get_string()
-    
-    images = wdh.get_images()
-    predictions = nnh.predict_images(captcha_string, images)
-    print(np.round(predictions.reshape((3,3)),2))
-
-    wdh.click_correct_images(predictions)
-
-    # check verification
-
-
-    
+    #wdh = wd_handler.Webdriver_Handler("https://account.proton.me/signup?plan=free&billing=12&minimumCycle=12&currency=EUR&language=en",available_models)
+    #wdh = wd_handler.Webdriver_Handler("https://freebitco.in/login",available_models)
--- automatic_classifier/wd_handler.py
+++ automatic_classifier/wd_handler.py
@@ -54,59 +54,99 @@ class Webdriver_Handler:
             fix_hairline=True,
         )
 
+        self.widget_state = 0
+
     
     def normalize_string(self, string):
         ratios = []
         for model_name in self.available_models:
             ratios.append(SequenceMatcher(a=string,b=model_name).ratio())
         print(ratios)
-        if max(ratios) > 0.9:
+        if max(ratios) > 0.5:
             return self.available_models[np.argmax(np.array(ratios))]
         return "--unidentified--"
 
-    def load_captcha(self):
+
+    def focus_on_root_frame(self):
+        self.wd.switch_to.default_content()
+        self.widget_state = 0
+        print("Switched to root iframe")
+
+    def focus_on_container_frame(self):
+        WebDriverWait(self.wd, self.timeout).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,"//iframe[contains(@src,'hcaptcha.com') and contains(@title,'checkbox')]")))
+        self.widget_state = 1
+        print("Switched to hCaptcha Container iframe")
+
+    def focus_on_challenge_frame(self):
+        WebDriverWait(self.wd, self.timeout).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,"//iframe[contains(@src,'hcaptcha.com') and contains(@title,'Main content')]")))
+        self.widget_state = 2
+        print("Switched to hCaptcha Challenge iframe")
+
+    def focus_on_frame(self, target_frame):
+        if target_frame == self.widget_state:
+            return
+        
+        if target_frame == 0:
+            self.focus_on_root_frame()
+            return
+        if target_frame == 1:
+            if self.widget_state == 2:
+                self.focus_on_root_frame()
+            self.focus_on_container_frame()
+            return
+        if target_frame == 2:
+            self.focus_on_root_frame()
+            self.focus_on_challenge_frame()
+            return
+
+
+    def load_captcha(self):        
         self.wd.get(self.website_url)
         print("Loaded Website")
 
         try:
-            WebDriverWait(self.wd, self.timeout).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,"//iframe[contains(@src,'hcaptcha.com') and contains(@title,'checkbox')]")))
+            self.focus_on_frame(1)
         except:
             print("No hCaptcha iframe found")
             return
         
-        print("Switched to hCaptcha Launch iframe")
+        # click hCaptcha container
         WebDriverWait(self.wd, self.timeout).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div"))).click()
         print("Launched hCaptcha")
 
-        self.wd.switch_to.default_content()
-
-    def focus_on_captcha_frame(self):
-        WebDriverWait(self.wd, self.timeout).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,"//iframe[contains(@src,'hcaptcha.com') and contains(@title,'Main content')]")))
-        print("Switched to hCaptcha Challenge iframe")
-
 
     def get_string(self):
+        self.focus_on_frame(2)
+
         print("Trying to get captcha string")
         try:
             captcha_str = self.wd.find_element(By.XPATH, "//span[contains(text(),'click') and contains(text(),'image')]").text
         except:
             captcha_str = self.wd.find_element(By.XPATH, "//span[contains(text(),'click') and contains(text(),'image')]").text
-        captcha_str = self.normalize_string(captcha_str)
         captcha_str = captcha_str.replace("Please click each image containing an ","")
         captcha_str = captcha_str.replace("Please click each image containing a ","")
+        captcha_str = self.normalize_string(captcha_str)
         captcha_str = captcha_str.replace(" ","_")
         print("Got hCaptcha string:",captcha_str)
         return captcha_str
     
     def get_images(self):
+        self.focus_on_frame(2)
+
         images = []
         print("Trying to get images")
         for i in range(9):
             try:
-                img_url = WebDriverWait(self.wd, self.timeout).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/div/div[2]/div["+str(i+1)+"]/div[2]/div"))).get_attribute("style").split("url(\"")[1].split("\") ")[0]
+                print(1)
+                img_url = WebDriverWait(self.wd, self.timeout).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/div/div[2]/div["+str(i+1)+"]/div[2]/div")))
+                print(img_url)
+                img_url = img_url.get_attribute("style")
+                print(img_url)
+                img_url = img_url.split("url(\"")[1]
+                print(img_url)
+                img_url = img_url.split("\") ")[0]
             except:
                 print("Cannot find Image",i+1)
-                return
             img_url = urljoin(self.website_url, img_url)
             img_content = requests.get(img_url, stream=True).content
             img = np.array(Image.open(BytesIO(img_content)))
@@ -121,6 +161,8 @@ class Webdriver_Handler:
         return images
     
     def click_correct_images(self, predictions):
+        self.focus_on_frame(2)
+
         corr = (predictions > 0.5)
         for i in range(9):
             if corr[i]:
@@ -136,7 +178,6 @@ class Webdriver_Handler:
         print("Trying to click next")
         WebDriverWait(self.wd, self.timeout).until(EC.element_to_be_clickable((By.XPATH, "//div[contains(@class, 'button-submit')]"))).click()
         print("Clicked next")
-        self.wd.switch_to.default_content()
         time.sleep(0.2)
         
         
@@ -149,4 +190,11 @@ class Webdriver_Handler:
         print("Clicked \"refresh\" button")
         time.sleep(0.2)
 
-  
+    def get_verification_status(self):
+        self.focus_on_frame(1)
+
+        check_div = WebDriverWait(self.wd, self.timeout).until(EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'check')]")))
+        print(check_div.value_of_css_property("display"))
+        if check_div.value_of_css_property("display") == "block":
+            return True
+        return False
