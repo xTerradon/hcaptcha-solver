a1fe52861397b8ceda4c7dd86920a527cc5686fe
xTerradon <l-b-n.02@gmx.de>
2023-08-09 16:59:38 +0300

    manual classifier

--- manual_classifier/manual_classifier.py
+++ manual_classifier/manual_classifier.py
@@ -4,87 +4,99 @@ from io import BytesIO
 from tkinter import ttk
 
 import numpy as np
+import pandas as pd
 from PIL import Image, ImageTk
+
+from copy import deepcopy as copy
+
+IMAGE_DIR = "./src/images/"
 
 
 class Manual_Classifier:
-    def __init__(self, name):
-        self.name = name
-        self.con = sqlite3.connect(self.name+".db")
-        self.id_history = []
-        print("Opened Table",self.name)
-    
-    def get_unique_captcha_strings(self):
-        return list(np.array(self.con.execute("SELECT DISTINCT captcha_string FROM "+self.name+" ORDER BY captcha_string").fetchall())[:,0])
-    
-    def get_data(self):
-        
-        if self.captcha_selection.get() == "ALL":
-            if len(self.id_history) == 0:
-                max_id = 0
-            else:
-                max_id = max(self.id_history)
-            self.number_total = self.con.execute("SELECT COUNT(rowid) FROM "+self.name).fetchone()[0]
-            self.number_unclassified = self.con.execute("SELECT COUNT(rowid) FROM "+self.name+" WHERE correct = 0").fetchone()[0]
-            element = self.con.execute("SELECT id, captcha_string, image, correct FROM "+self.name+" WHERE id > "+str(max_id)+" ORDER BY captcha_string LIMIT 1").fetchone()
-        else:
-            cs = self.captcha_selection.get()
-            print("Searching for images with",cs)
-            self.number_total = self.con.execute("SELECT COUNT(rowid) FROM "+self.name+" WHERE captcha_string = \'"+cs+"\'").fetchone()[0]
-            self.number_unclassified = self.con.execute("SELECT COUNT(rowid) FROM "+self.name+" WHERE captcha_string = \'"+cs+"\' AND correct = 0").fetchone()[0]
-            element = self.con.execute("SELECT id, captcha_string, image, correct FROM "+self.name+" WHERE captcha_string = \'"+cs+"\' AND correct = 0 ORDER BY captcha_string LIMIT 1").fetchone()
-        if element == None:
-            print("Could not resolve",self.captcha_selection.get())
-            return
-        self.img_id = element[0]
-        self.id_history.append(self.img_id)
-        self.captcha_string = element[1]
-        img = Image.open(BytesIO(element[2])).resize((200,200))
-        self.captcha_image = ImageTk.PhotoImage(img)
-        self.current_correct = element[3]
-    
-    def mainloop(self):
+    def __init__(self, db_handler):
+        self.db_handler = db_handler
+        self.id_history = pd.DataFrame([], columns=["id","action"])
 
         self.window = tk.Tk()
         self.window.bind("<Key>",self.key_input)
 
         self.info_label = tk.Label(self.window, text="Loading...")
         self.captcha_selection = tk.StringVar()
+        self.captcha_selection.set(self.db_handler.get_most_unsolved_captcha_string())
+
         self.captcha_selector = ttk.Combobox(self.window, textvariable=self.captcha_selection)
-        print(self.get_unique_captcha_strings())
-        self.captcha_selector['values'] = ["ALL"] + self.get_unique_captcha_strings()
+        self.captcha_selector['values'] =  self.db_handler.get_captcha_strings()
         self.captcha_selector.config(state="readonly")
-        self.captcha_selector.bind('<<ComboboxSelected>>', self.refresh_data)
-        self.captcha_selection.set("ALL")
+        self.captcha_selector.bind('<<ComboboxSelected>>', lambda h : self.update_data())
+
         self.captcha_label = tk.Label(self.window, text="Loading...")
 
-        self.img_label = tk.Label()
+        self.decision_frame = tk.Frame(self.window)
+        self.set_images()
+        self.set_info_label()
 
-        decision_frame = tk.Frame(self.window)
+        self.menu_frame = tk.Frame(self.window)
+        self.go_back_button = tk.Button(self.menu_frame, text="Back")
+        self.show_solved_button = tk.Button(self.menu_frame, text="Show Solved")
+        self.continue_button = tk.Button(self.menu_frame, text="Continue")
 
-        yes_button = tk.Button(decision_frame, text="YES", command=lambda : self.update_correct_value(1), width=15, height=5)
-        zero_button = tk.Button(decision_frame, text="0", command=lambda : self.update_correct_value(0), width=5, height=5)
-        no_button = tk.Button(decision_frame, text="NO", command=lambda : self.update_correct_value(-1), width=15, height=5)
-
-        revise_button = tk.Button(self.window, text="GO BACK", command=lambda : self.load_last(), width=15, height=2)
 
         self.captcha_selector.pack(padx=5, pady=5)
         self.info_label.pack(padx=50)
         self.captcha_label.pack(padx=20, pady=5)
-        self.img_label.pack(padx=20, pady=5)
-        
-        yes_button.pack(side="left", padx=1, pady=1)
-        zero_button.pack(side="left", padx=1, pady=1)
-        no_button.pack(side="right", padx=1, pady=1)
-        decision_frame.pack(padx=5, pady=5)
+        self.decision_frame.pack(padx=5, pady=5)
+        self.menu_frame.pack(padx=5, pady=5)
 
-        revise_button.pack(padx=20, pady=5)
-
-        self.get_data()
-        self.load_data_to_ui()
+        self.go_back_button.pack(side=tk.LEFT, padx=5, pady=5)
+        self.show_solved_button.pack(side=tk.LEFT, padx=5, pady=5)
+        self.continue_button.pack(side=tk.LEFT, padx=5, pady=5)
 
         tk.mainloop()
     
+    def update_data(self):
+        self.set_images()
+        self.set_info_label()
+        self.window.update()
+    
+    def set_images(self):
+        cs = self.captcha_selection.get()
+        print(f"Setting images for {cs}")
+
+        image_paths = self.db_handler.get_unsolved_images_paths(cs, number=9)
+        images_pil = [Image.open(IMAGE_DIR+image_path).resize((200,200)) for image_path in image_paths]
+        images_tk = [ImageTk.PhotoImage(image_pil, master=self.decision_frame) for image_pil in images_pil]
+
+        self.image_buttons = []
+        self.image_frames = []
+        for i in range(9):
+            self.image_frames.append(tk.Frame(
+                self.decision_frame,
+                highlightbackground="red",
+                highlightthickness=10,
+                bd=0, 
+                height=210, 
+                width=210))
+            self.image_buttons.append(tk.Button(
+                self.image_frames[-1], 
+                image=images_tk[i],
+                command=lambda : self.turn_red(self.image_frames[-1])))
+            self.image_buttons[-1].image = images_tk[i]
+
+            self.image_buttons[-1].pack()
+            self.image_frames[-1].grid(row=i//3, column=i%3, padx=5, pady=5)
+
+
+    
+    def turn_red(self, i):
+        print(i)
+        i.config({"highlightbackground":"green"})
+    
+    def set_info_label(self):
+        solvedinfo = self.db_handler.get_solved_unsolved_for_captcha_string(self.captcha_selection.get())
+        self.info_label.config(text = f"Solved: {solvedinfo['solved']}, Unsolved: {solvedinfo['unsolved']}, Percentage: {round(solvedinfo['solved']/(solvedinfo['solved']+solvedinfo['unsolved'])*100,2)}%")
+        self.captcha_label.config(text=self.captcha_selection.get())
+
+
     def key_input(self, event):
         if event.char == 'y':
             self.update_correct_value(1)
@@ -94,43 +106,3 @@ class Manual_Classifier:
             self.load_last()
         else:
             print("KEYSTROKE NOT RECOGNIZED")
-    
-    def refresh_data(self, _):
-        self.get_data()
-        self.load_data_to_ui()
-
-    def update_correct_value(self, value):
-        self.set_correct_value(value)
-        self.get_data()
-        self.load_data_to_ui()
-    
-    def load_data_to_ui(self):
-        self.info_label.config(text = "ID: "+str(self.img_id)+" "+str(self.current_correct)+" | Total: "+str(self.number_total)+" | Labeled: "+str(self.number_total-self.number_unclassified)+" "+str(round(100*(self.number_total-self.number_unclassified)/self.number_total))+"%")
-        self.captcha_label.config(text=self.captcha_string)
-        self.img_label.config(image = self.captcha_image)
-        self.img_label.image = self.captcha_image
-        self.window.update()
-    
-    def load_last(self):
-        self.set_correct_value(0)
-        if len(self.id_history) > 1:
-            self.img_id = self.id_history[-2]
-        else:
-            print("No history")
-            return
-        self.set_correct_value(0)
-        self.id_history = self.id_history[:-2]
-        self.get_data()
-
-        self.load_data_to_ui()
-        
-
-    def set_correct_value(self, value):
-        if value != None:
-            print("Setting",self.img_id,"to",value)
-            self.con.execute("UPDATE "+self.name+" SET correct="+str(value)+" WHERE id="+str(self.img_id))
-            self.con.commit()
-
-
-mc = Manual_Classifier("captchas")
-mc.mainloop()
