adef03c96ccca2c9872059107127bf7b55019ca1
xTerradon <l-b-n.02@gmx.de>
2023-08-15 22:11:35 +0300

    dev + testing

--- testing/test.py
+++ testing/test.py
@@ -1,17 +1,13 @@
 from hcaptcha_solver import hcaptcha_solver
 from selenium import webdriver
 
-# create Captcha_Solver object to load ML models
-captcha_solver = hcaptcha_solver.Captcha_Solver(verbose=True)
-
-# create webdriver and access website with hCaptcha
+# setup chromedriver in english (other languages not supported yet)
 options = webdriver.ChromeOptions()
-options.add_experimental_option('prefs', {'intl.accept_languages': 'en,en_US'}) # set to english
+options.add_experimental_option('prefs', {'intl.accept_languages': 'en,en_US'})
 driver = webdriver.Chrome(options=options)
-driver.get("https://accounts.hcaptcha.com/demo")
+driver.get("https://accounts.hcaptcha.com/demo") # open any url with hCaptcha
 
-# check for hCaptcha
-captcha_solver.is_captcha_present(driver)
-
-# solve hCaptcha
-captcha_solver.solve_captcha(driver)
+# create Captcha_Solver object and solve captcha
+captcha_solver = hcaptcha_solver.Captcha_Solver(verbose=True)
+captcha_solver.is_captcha_present(driver) # returns True
+captcha_solver.solve_captcha(driver) # solves the hCaptcha
