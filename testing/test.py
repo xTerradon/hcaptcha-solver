f762b50c32c61c2f116d7380a939eb07653359c3
ludwig7685 <139857057+ludwig7685@users.noreply.github.com>
2023-08-16 11:51:59 +0200

    test.py to solve both pages of the hcaptcha
    solves both pages of hcaptcha.
    leaves selenium open after finishing.
    
    ~ wrote with chatgpt, becuase i was to lazy to write, lol

--- testing/test.py
+++ testing/test.py
@@ -1,13 +1,31 @@
 from hcaptcha_solver import hcaptcha_solver
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-# setup chromedriver in english (other languages not supported yet)
-options = webdriver.ChromeOptions()
-options.add_experimental_option('prefs', {'intl.accept_languages': 'en,en_US'})
-driver = webdriver.Chrome(options=options)
-driver.get("https://accounts.hcaptcha.com/demo") # open any url with hCaptcha
+def solve_hcaptcha_page(driver):
+    captcha_solver = hcaptcha_solver.Captcha_Solver(verbose=True)
+    captcha_solver.is_captcha_present(driver)  # Returns True
+    captcha_solver.solve_captcha(driver)  # Solves the hCaptcha
 
-# create Captcha_Solver object and solve captcha
-captcha_solver = hcaptcha_solver.Captcha_Solver(verbose=True)
-captcha_solver.is_captcha_present(driver) # returns True
-captcha_solver.solve_captcha(driver) # solves the hCaptcha
+def main():
+    # setup chromedriver in English (other languages not supported yet)
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('prefs', {'intl.accept_languages': 'en,en_US'})
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://accounts.hcaptcha.com/demo")  # Open any URL with hCaptcha
+
+    try:
+        # Solve both pages of hCaptcha
+        solve_hcaptcha_page(driver)
+        solve_hcaptcha_page(driver)
+
+        input("Press Enter to close the browser tab...")
+    except Exception as e:
+        print("An error occurred:", e)
+    finally:
+        driver.quit()
+
+if __name__ == "__main__":
+    main()
